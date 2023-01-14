fbf64c5d4cdacc6abb44855980bd1132d225957f
evagpulido <117755782+evagpulido@users.noreply.github.com>
2023-01-14 19:29:40 +0100

    Create p2.py

---
+++ Apartado1/p2.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+
+
+import logging 
+import sys 
+from subprocess import call
+import os
+
+
+logging.basicConfig(level=logging.DEBUG) 
+logger = logging.getLogger('pc2')
+
+
+# GUARDAMOS EL VALOR DE LA VARIABLE DE ENTORNO
+numeroGrupo = os.environ.get("GROUP_NUMBER")
+
+# COMANDOS NECESARIOS PARA LA PUESTA EN MARCHA DE LA APLICACIÓN	
+call(["git", "clone", "https://github.com/CDPS-ETSIT/practica_creativa2.git"])
+logger.debug("Repositorio clonado correctamente") 
+
+# ACTUALIZAMOS EL SISTEMA
+call(["sudo", "apt-get", "update"])
+logger.debug("Sistema actualizado") 
+
+# INSTALAMOS PIP
+call(["sudo", "apt-get", "install", "python3-pip"])
+logger.debug("pip se ha instalado correctamente") 
+
+# AÑADIMOS LOS REQUIREMENTS NECESARIOS
+call(["pip3", "install", "-r", "practica_creativa2/bookinfo/src/productpage/requirements.txt"])
+logger.debug("Las dependencias han sido instaladas") 
+
+
+# MODIFICAMOS EL TITULO DE LA APLICACION
+call(["cp", "practica_creativa2/bookinfo/src/productpage/templates/productpage.html", "practica_creativa2/bookinfo/src/productpage/templates/productpageCopia.html"])
+fCopia = open("practica_creativa2/bookinfo/src/productpage/templates/productpageCopia.html", 'r')
+fOriginal = open("practica_creativa2/bookinfo/src/productpage/templates/productpage.html", 'w')
+for line in fCopia:
+    if "{% block title %}Simple Bookstore App{% endblock %}" in line:
+        fOriginal.write("{% block title %}Simple Bookstore App - GRUPO " + numeroGrupo + "{% endblock %}")
+    else:
+        fOriginal.write(line)
+fOriginal.close()
+fCopia.close()
+logger.debug("Se ha actualizado el título de la aplicación") 
+
+
+# ARRANCAMOS LA APLICACIÓN
+call(["python3", "practica_creativa2/bookinfo/src/productpage/productpage_monolith.py", "9080"])
+logger.debug("La aplicación se está ejecuntando correctamente. Puede acceder a ella desde https://<ip-publica>:9080/productpage") 
+
+
